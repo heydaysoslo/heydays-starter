01ec3feacb38b4cba8d9ccd57181cd68274b3880
mittnavnermike <mikepatterson@Mikes-MBP-2.heydays.local>
2019-08-28 16:11:51 +0200

    Added menu to footer

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -1,14 +1,38 @@
 import React from 'react'
+
+import { graphql, useStaticQuery } from 'gatsby'
+
 import Newsletter from './Newsletter'
+import LinkResolver from './LinkResolver'
 
 const Footer = () => {
+  const data = useStaticQuery(query)
+  const menu = data?.sanityMenu?._rawItem
   return (
     <div className="Footer">
       <div className="Footer__newsletter">
         <Newsletter />
       </div>
+      <ul className="Footer__menu">
+        {menu &&
+          menu.map(item => (
+            <li className="Footer__menu-item">
+              <LinkResolver key={item._key} data={item.link}>
+                {item.title || item.link.title}
+              </LinkResolver>
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
 
 export default Footer
+
+export const query = graphql`
+  {
+    sanityMenu(_id: { eq: "menu-footerMenu" }) {
+      _rawItem(resolveReferences: { maxDepth: 10 })
+    }
+  }
+`
