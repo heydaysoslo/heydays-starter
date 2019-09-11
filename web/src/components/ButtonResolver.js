2220ad80c6f9b87fec1b7629650ddeea8cf3c13f
mittnavnermike <mikepatterson@Mikes-MBP-2.heydays.local>
2019-09-11 13:30:54 +0200

    :bug: fixed type secondary issue

--- web/src/components/ButtonResolver.js
+++ web/src/components/ButtonResolver.js
@@ -3,17 +3,10 @@ import LinkResolver from './LinkResolver'
 import cc from 'classcat'
 
 const ButtonResolver = ({ button, className }) => {
-  // console.log(button)
   const title = button?.link?.title || button?.link?.reference?.title
   const url = button?.link?.externalLink?.url || button?.link?.reference
   const openInNewTab = button?.link?.externalLink?.blank
   const variant = button?.type
-  // console.log(title, variant)
-  // if (variant) {
-  //   console.log('variant is true', variant)
-  // } else {
-  //   console.log('variant is false', variant)
-  // }
   return (
     <>
       {title && url && (
@@ -22,7 +15,6 @@ const ButtonResolver = ({ button, className }) => {
             Button: true,
             'Button--link': true,
             [`Button--${variant}`]: variant,
-            'Button--secondary': !variant,
             [className]: className
           })}
           data={url}
