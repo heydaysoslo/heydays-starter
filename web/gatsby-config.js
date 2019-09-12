ac4cbcabc05d539eaae2ace9f8a59381ff2d212b
mittnavnermike <mikepatterson@Mikes-MBP-2.heydays.local>
2019-09-12 17:43:05 +0200

    removed cdn and token from gatsby-source-sanity

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -8,8 +8,8 @@ module.exports = new Promise((resolve, reject) => {
   const client = sanityClient({
     projectId: process.env.SANITY_PROJECT_ID,
     dataset: process.env.SANITY_DATASET_PROD,
-    token: process.env.SANITY_TOKEN, // or leave blank to be anonymous user
-    useCdn: true // `false` if you want to ensure fresh data
+    // token: process.env.SANITY_TOKEN, // or leave blank to be anonymous user
+    useCdn: false // `false` if you want to ensure fresh data
   })
 
   const query = `
