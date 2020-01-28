8fb7483be5b80b85fe2acf4f5848ee412a8fbb0a
mittnavnermike <mikepatterson@Mikes-MacBook-Pro-2.local>
2020-01-28 22:38:49 +0100

    :art: removed hover styling for accordion

--- web/src/components/Accordion.js
+++ web/src/components/Accordion.js
@@ -155,7 +155,6 @@ StyledAccordion.Item = styled(AccordionItem)(
 
         &:hover {
           background: ${color.darken(theme.colors.primary, 0.5)};
-          opacity: 0.5;
         }
 
         &:focus {
