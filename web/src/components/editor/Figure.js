b5338c617713dfb63725c3e4b11f7500279456b4
mittnavnermike <mikepatterson@Mikes-MBP-2.heydays.local>
2020-01-29 09:50:44 +0100

    convert figure to sc

--- web/src/components/editor/Figure.js
+++ web/src/components/editor/Figure.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import SanityImage from './SanityImage'
+import styled, { css } from 'styled-components'
 
-const Figure = ({ node, aspectRatio }) => {
+const Figure = ({ node, aspectRatio, className }) => {
   if (!node || !node.asset) {
     return null
   }
   return (
-    <figure className="Figure">
+    <figure className={className}>
       <SanityImage key={node.asset?.id} node={node} aspectRatio={aspectRatio} />
       {node.caption && (
         <figcaption className="Figure__caption">{node.caption}</figcaption>
@@ -15,4 +16,9 @@ const Figure = ({ node, aspectRatio }) => {
   )
 }
 
-export default Figure
+export default styled(Figure)(
+  ({ theme }) => css`
+    width: 100%;
+    max-width: 100%;
+  `
+)
