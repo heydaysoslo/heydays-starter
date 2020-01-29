40d05867743405fdec604d23a891c0466c723a13
mittnavnermike <mikepatterson@Mikes-MBP-2.heydays.local>
2020-01-29 09:52:52 +0100

    start block grid

--- web/src/components/pagebuilder/CardSection.js
+++ web/src/components/pagebuilder/CardSection.js
@@ -2,23 +2,41 @@ import React from 'react'
 import Grid from '../Grid'
 
 import Card from '../Card'
+import BlockGrid from '../BlockGrid'
 
 const CardSection = ({ cardsList = [], ...props }) => {
   return (
-    <Grid columns={{ sm: 1, md: 3 }} margin="y">
-      {cardsList.map(card => {
-        const { content, cardOverride } = card
-        return (
-          <Card
-            key={card?._key}
-            title={cardOverride?.title || content?.title}
-            image={cardOverride?.image || content?.mainImage}
-            excerpt={cardOverride?.content || content?.excerpt}
-            link={cardOverride?.link || content}
-          />
-        )
-      })}
-    </Grid>
+    <>
+      <Grid columns={{ sm: 1, md: 3 }} margin="y">
+        {cardsList.map(card => {
+          const { content, cardOverride } = card
+          return (
+            <Card
+              key={card?._key}
+              title={cardOverride?.title || content?.title}
+              image={cardOverride?.image || content?.mainImage}
+              excerpt={cardOverride?.content || content?.excerpt}
+              link={cardOverride?.link || content}
+            />
+          )
+        })}
+      </Grid>
+      <h1>BLOCK GRID</h1>
+      <BlockGrid columns={{ sm: 1, md: 3 }} gap="20px">
+        {cardsList.map(card => {
+          const { content, cardOverride } = card
+          return (
+            <Card
+              key={card?._key}
+              title={cardOverride?.title || content?.title}
+              image={cardOverride?.image || content?.mainImage}
+              excerpt={cardOverride?.content || content?.excerpt}
+              link={cardOverride?.link || content}
+            />
+          )
+        })}
+      </BlockGrid>
+    </>
   )
 }
 
