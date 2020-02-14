ab7cdbe225dac1d8bcc167e61744135a9cd356de
mittnavnermike <mikepatterson@Mikes-MBP-2.heydays.local>
2020-02-14 17:42:05 +0100

    :sparkles: added tabs and media browser to sanity

--- cms/custom/components/Logo.js
+++ cms/custom/components/Logo.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import '../fonts/fonts.css'
+import "../fonts/fonts.css";
 
 const style = {
-  minHeight: '25px',
-  transform: 'translateY(2px)'
-}
+  minHeight: "25px",
+  transform: "translateY(2px)"
+};
 
 export default class Logo extends Component {
   render() {
     return (
       <div>
-        PLACE LOGO HERE
-        {/* <svg
+        <svg
           version="1.1"
-          viewBox="0 0 252.5 112.7"
+          baseProfile="tiny"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 702.34 864.8"
           xmlSpace="preserve"
-          style={style}
         >
-          <path
-            fill="#C7C1BA"
-            d="M131.9,49.9v9.2c0,10.2-4.4,16.7-18.7,16.7s-18.7-6.5-18.7-16.7v-9.2c0-12.5,4.7-16.8,18.9-16.8
-	C126,33.1,131.9,37.2,131.9,49.9 M240.9,79.1c-5.2,5.2-11.1,7.1-19.5,7.1c-16.2,0-27.5-13.2-27.5-29.9V23.9h11.6v35.2
-	c0,10.6,4.7,16.7,18.1,16.7c12.6,0,17.4-4.4,17.4-16.7V23.9h11.6v60.1c0,17.9-14.3,28.7-30.1,28.7H215V102h8.6
-	c11.9,0,17.3-5.2,17.3-15.5V79.1z M0,54.1C0,69.7,11.9,85,29.6,85h9V74.4h-9c-12.2,0-16.9-4.1-16.9-16.4v-8
-	c0-12.6,6.1-16.8,18.9-16.8s18.9,4.1,18.9,16.8v18c0,11.5,6.7,17.1,17.4,17.1h8.5V74.4H62.3V54.1c0-18.6-11.2-31.4-30.5-31.4
-	C11.9,22.7,0,37.2,0,54.1 M170.2,74.4V0h-11.6v67.9c0,11.5,6.7,17.1,17.1,17.1h9.2V74.4H170.2z M115.4,22.7
-	c-9.1,0-16.3,3.2-20.8,8.3V0H83v54.1c0,22.9,14.4,32,30.3,32c18.2,0,31.2-14.9,31.2-31.7C144.5,37.2,131.7,22.7,115.4,22.7"
-          />
-        </svg> */}
+          <g>
+            <polygon
+              points="491.87,314.97 210.48,314.97 210.48,0 0,0 0,864.8 210.48,864.8 210.48,518.43 491.87,518.43 491.87,864.8
+		702.34,864.8 702.34,0 491.87,0 	"
+            />
+          </g>
+        </svg>
       </div>
-    )
+    );
   }
 }
