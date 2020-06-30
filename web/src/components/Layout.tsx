ff92f0f0f220f1aa9c7d407e06757b67a4d8619e
mittnavnermike <niiimon@gmail.com>
2020-06-30 11:37:26 +0200

    FontPreloading as comp in layout

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,79 +1,28 @@
 import React, { useState } from 'react'
 
-import { getUrl } from '../../heydays-config'
-import { blocksToText } from '../utils/sanityHelpers'
-
 import Header from './Header'
 import Footer from './Footer'
 import SEO from './SEO'
 import { ThemeProvider } from 'styled-components'
 import theme, { darkTheme } from '../styles/themes'
 import { GlobalStyle } from '../styles/utilities/Global'
-import Helmet from 'react-helmet'
+import FontPreLoading from './FontPreLoading'
 
-const Layout = props => {
+const Layout = ({ page, children }) => {
   const themes = [theme, darkTheme]
   const [number, setNumber] = useState(0)
 
   return (
     <ThemeProvider theme={themes[number % themes.length]}>
       <div className="Site">
-        <FontPreloadLoading />
-        {props && (
-          <SEO
-            id={props.id}
-            getUrl={getUrl}
-            type={props._type}
-            slug={props._rawSlug?.current || ''}
-            seo={props.seo}
-            title={props.title}
-            image={props?.seo?.image || props.mainImage}
-            description={props._rawExcerpt && blocksToText(props._rawExcerpt)}
-            authors={props?.authors}
-            publishDate={props?.dateString}
-          />
-        )}
+        <FontPreLoading />
+        <SEO page={page} />
         <Header />
-        <div className="Site__content">{props.children}</div>
+        <div className="Site__content">{children}</div>
         <Footer />
         <GlobalStyle />
       </div>
     </ThemeProvider>
-  )
-}
-
-const FontPreloadLoading = () => {
-  return (
-    <Helmet>
-      <link
-        rel="preload"
-        as="font"
-        href="/fonts/SuisseIntl-Regular-WebM.woff2"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        as="font"
-        href="/fonts/SuisseIntl-Medium-WebM.woff2"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        as="font"
-        href="/fonts/SuisseWorks-Regular-WebS.woff2"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preload"
-        as="font"
-        href="/fonts/SuisseNeue-Regular-WebS.woff2"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-    </Helmet>
   )
 }
 
