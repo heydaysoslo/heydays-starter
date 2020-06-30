ec46c4e8d41b55bc00985006ce0ff11083dc6e9c
mittnavnermike <niiimon@gmail.com>
2020-06-30 11:47:33 +0200

    :package: SEO comp

--- web/src/components/SEO.js
+++ web/src/components/SEO.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { graphql, useStaticQuery } from 'gatsby'
-import {
-  GatsbySeo,
-  ArticleJsonLd,
-  LocalBusinessJsonLd
-} from 'gatsby-plugin-next-seo'
+import { GatsbySeo, ArticleJsonLd } from 'gatsby-plugin-next-seo'
+import { getOgType } from '../routes'
+import { toPlainText } from '../utils/helpers'
+import useLinkResolver from './hooks/useLinkResolver'
 
-const truncateString = string => {
+/*
+Opengraph: https://ogp.me/#types
+
+TODO:
+
+Add JsonLd types for
+- Blog
+- BlogPost
+- Article (done)
+- Local Business
+- Logo
+- Breadcrumb (maybe)
+Docs: https://www.gatsbyjs.org/packages/gatsby-plugin-next-seo/
+
+All pages should have a "last updated" tag (?)
+
+*/
+
+const truncateString = (string, maxLength = 158) => {
   if (!string) return null
-  const maxLength = 50
   if (string.length < maxLength) {
     return string
   }
-  return `${string.substring(0, 50)}…`
+  return `${string.substring(0, maxLength)}…`
 }
 
 const getLastValidOverride = (arr, key) => {
@@ -26,71 +42,118 @@ const getLastValidOverride = (arr, key) => {
   return val
 }
 
-const getTitleFromTemplate = (template, title) => {
-  return template.replace(/%s/g, title)
+// const getTitleFromTemplate = (template, title) => {
+//   return template.replace(/%s/g, title)
+// }
+
+// const getDescription = (page, sanitySiteSettings) => {
+//   console.log(page.seo)
+//   if (sanitySiteSettings.description) return ''
+// }
+
+const normalizePageKeys = page => {
+  // Normalize page props to seo props
+  const normalizedPage = Object.assign({}, page)
+  if (!normalizedPage.description) {
+    if (normalizedPage.body) {
+      normalizedPage.description = toPlainText(normalizedPage.body)
+    }
+    // What about pagebuilders?
+  }
+  if (normalizedPage.description) {
+    normalizedPage.description = truncateString(normalizedPage.description)
+  }
+  return normalizedPage
 }
 
-const SEO = props => {
+const SEO = ({ page }) => {
   const data = useStaticQuery(query)
+  const canonical = useLinkResolver(page, { canonical: true })
   const { sanityCompanyInfo, sanitySiteSettings } = data
-  const { type, slug, getUrl, authors, publishDate } = props
-
-  if (!type || (type !== 'frontpage' && !slug)) {
-    console.info(
-      `Type or slug is missing. We need this to make the SEO. Type: ${type} and Slug: ${slug}`
-    )
+  if (!page) {
     return null
   }
 
-  const titleTemplate = `%s » ${sanityCompanyInfo?.name}`
+  const ogType = getOgType(page)
   const locale = sanitySiteSettings?.locale || 'en_US'
-  const ogType = type === 'article' ? 'article' : 'website'
-  const canonicalUrl = `${sanitySiteSettings?.siteUrl}${getUrl(type, slug)}`
+  // const { type, slug, getUrl, authors, publishDate, id } = props
+
+  // if (!type) {
+  //   console.info(`Type is missing. We need this to make the SEO. Type: ${type}`)
+  //   return null
+  // }
+
+  // const ogType = type === 'article' ? 'article' : 'website'
 
   /**
    * Get meta tag overrides recursively, starting from global site settings
    */
-  const sources = [sanitySiteSettings?.seo, props, props.seo]
-  const title = getLastValidOverride(sources, 'title')
+  const pageExtract = normalizePageKeys(page)
+  const sources = [sanitySiteSettings?.seo, pageExtract, page.seo]
   const description = getLastValidOverride(sources, 'description')
+  const title = getLastValidOverride(sources, 'title')
   const image = getLastValidOverride(sources, 'image')
 
-  const tabTitle =
-    type === 'frontpage'
-      ? title
-      : getTitleFromTemplate(titleTemplate, truncateString(title))
+  const openGraph = {
+    url: canonical,
+    title: title,
+    description: description,
+    type: ogType,
+    locale: locale,
+    site_name: sanityCompanyInfo?.name,
+    images: image?.asset?.fixed && [
+      {
+        url: image.asset.fixed.src,
+        width: image.asset.fixed.width,
+        height: image.asset.fixed.height
+      }
+    ]
+  }
+
+  /**
+   * Set all props to objects to allow debugging
+   */
+  const gatsbySeoProps = {
+    titleTemplate: `%s » ${sanityCompanyInfo?.name}`,
+    title: title,
+    canonical: canonical,
+    openGraph: openGraph,
+    facebook: { appId: sanitySiteSettings?.facebookAppId },
+    twitter: {
+      handle: `@${sanityCompanyInfo?._rawSocial?.twitter.split('/').pop()}`,
+      cardType: 'summary_large_image'
+    }
+  }
+
+  //
+  const articleJsonProps = {
+    url: canonical,
+    headline: title,
+    description,
+    images: image?.asset?.fixed && [
+      {
+        '@type': 'ImageObject',
+        url: image.asset.fixed.src,
+        width: image.asset.fixed.width,
+        height: image.asset.fixed.height
+      }
+    ],
+    datePublished: page.publishDate,
+    publisherName: sanityCompanyInfo?.name,
+    publisherLogo: null,
+    authorName: page?.authors && page?.authors[0]?.person?.name,
+    authorType: 'Person'
+  }
 
   return (
     <>
-      <GatsbySeo
-        title={tabTitle}
-        description={description}
-        canonical={canonicalUrl}
-        openGraph={{
-          url: canonicalUrl,
-          title: tabTitle,
-          description: description,
-          type: ogType,
-          locale: locale,
-          images: image?.asset?.fixed && [
-            {
-              url: image.asset.fixed.src,
-              width: image.asset.fixed.width,
-              height: image.asset.fixed.height
-            }
-          ],
-          site_name: sanityCompanyInfo?.name
-        }}
-        twitter={{
-          handle: `@${sanityCompanyInfo?._rawSocial?.twitter.split('/').pop()}`,
-          cardType: 'summary_large_image'
-        }}
-        facebook={{ appId: sanitySiteSettings?.facebookAppId }}
-      />
-
+      {/* <pre>{JSON.stringify(gatsbySeoProps, null, 2)}</pre>
+      <pre>{JSON.stringify(articleJsonProps, null, 2)}</pre> */}
+      <GatsbySeo {...gatsbySeoProps} />
+      {ogType === 'article' && <ArticleJsonLd {...articleJsonProps} />}
       {/* Rich result for article */}
       {/* https://schema.org/LocalBusiness */}
-      {type === 'article' && (
+      {/* {type === 'article' && (
         <ArticleJsonLd
           url={canonicalUrl && canonicalUrl}
           headline={title && title}
@@ -130,14 +193,14 @@ const SEO = props => {
             }, [])
           }}
         />
-      )}
+      )} */}
 
       {/*
       Rich result for article.
       Only show on frontpage.
       https://schema.org/LocalBusiness
        */}
-      {type === 'frontpage' && (
+      {/* {sanitySiteSettings?._rawFrontpage?.id === id && (
         <LocalBusinessJsonLd
           type="LocalBusiness"
           id={sanitySiteSettings?.siteUrl || ''}
@@ -169,7 +232,7 @@ const SEO = props => {
             }
           ]}
         />
-      )}
+      )} */}
       <Helmet>
         <html lang={locale.includes('_') ? locale.split('_')[0] : locale} />
       </Helmet>
@@ -180,31 +243,32 @@ const SEO = props => {
 export default SEO
 
 const query = graphql`
+  fragment SEO on SanitySeo {
+    title
+    description
+    image {
+      asset {
+        fixed(width: 1200, height: 630) {
+          ...GatsbySanityImageFixed
+        }
+      }
+    }
+  }
   {
     sanityCompanyInfo {
-      _rawOffices(resolveReferences: { maxDepth: 10 })
       _rawSocial(resolveReferences: { maxDepth: 10 })
-      _rawAddress(resolveReferences: { maxDepth: 10 })
       email
       name
       phone
-      lng
-      lat
     }
-    sanitySiteSettings {
+    sanitySiteSettings(_id: { eq: "siteSettings" }) {
       locale
       siteUrl
       facebookAppId
-      # _rawFrontpage(resolveReferences: { maxDepth: 10 })
+      _rawFrontpage(resolveReferences: { maxDepth: 10 })
       _rawSeo(resolveReferences: { maxDepth: 10 })
       seo {
-        image {
-          asset {
-            fixed(width: 1200, height: 630) {
-              ...GatsbySanityImageFixed
-            }
-          }
-        }
+        ...SEO
       }
     }
   }
